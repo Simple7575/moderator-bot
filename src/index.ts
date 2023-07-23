f29435890d47aff6899b2a336b19173f38adf34f
Simple <varg616@gmail.com>
2023-07-23 13:52:22 +0400

    Changes for cyclic deployment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { webhookCallback } from "grammy";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
@@ -12,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 app.use(helmet());
+app.use(`/${BOT_TOKEN}`, webhookCallback(bot, "express"));
+
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome");
+});
 
 app.use("/bot", botRouter);
 
@@ -22,8 +28,11 @@ app.listen(PORT, async () => {
     } else {
         if (!BOT_TOKEN) throw new Error("Bot Token needed.");
         if (!WEB_URI) throw new Error("Web URI needed");
-        await bot.api.setWebhook(`${WEB_URI || "http://localhost:5000"}/bot/bot${BOT_TOKEN}`, {
+        await bot.api.setWebhook(`${WEB_URI}/${BOT_TOKEN}`, {
             drop_pending_updates: true,
         });
+        // await bot.api.setWebhook(`${WEB_URI || "http://localhost:5000"}/bot/bot${BOT_TOKEN}`, {
+        //     drop_pending_updates: true,
+        // });
     }
 });
